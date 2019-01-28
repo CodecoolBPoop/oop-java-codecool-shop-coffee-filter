a3dd8f94dc7cfc5a71fbc62c615378a4d1e891bf
gaborKriger <regirck@gmail.com>
2019-01-28 14:54:49 +0100

    registration and login page, little session

--- src/main/java/com/codecool/shop/controller/RegistrationController.java
+++ src/main/java/com/codecool/shop/controller/RegistrationController.java
@@ -13,26 +13,24 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/login"})
-public class LoginController extends HttpServlet {
+@WebServlet(urlPatterns = {"/registration"})
+public class RegistrationController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        engine.process("session/login.html", context, resp.getWriter());
+        engine.process("session/registration.html", context, resp.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username=req.getParameter("username");
         String password=req.getParameter("password");
+        String email=req.getParameter("email");
 
-        // if post username equals the sql username
-        HttpSession session=req.getSession();
-        session.setAttribute("username",username);
-        // send error-message
+        // variables save to database user table
 
         HttpServletResponse httpResponse = (HttpServletResponse) resp;
         httpResponse.sendRedirect("/");
