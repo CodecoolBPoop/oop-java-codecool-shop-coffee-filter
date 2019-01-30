db9b8612005447b08f9fcb23d776a7b7e994062f
Nora Filep <filepnora@gmail.com>
2019-01-30 14:34:14 +0100

    exception throw added

--- src/main/java/com/codecool/shop/dao/DataBaseConnect.java
+++ src/main/java/com/codecool/shop/dao/DataBaseConnect.java
@@ -20,7 +20,7 @@ public abstract class DataBaseConnect {
     protected static ResultSet resultSet = null;
 
 
-    public Connection getDbConnection() throws SQLException {
+    public Connection getDbConnection() throws ClassNotFoundException, SQLException {
         return DriverManager.getConnection(
                 DB_URL,
                 DB_USER,
@@ -41,17 +41,5 @@ public abstract class DataBaseConnect {
 
     public void setResultSet(ResultSet resultSet) {
         this.resultSet = resultSet;
-    }
-
-
-    public void executeQuery(String query) {
-        try (Connection connection = getDbConnection();
-             Statement statement =connection.createStatement();
-        ){
-            statement.execute(query);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
