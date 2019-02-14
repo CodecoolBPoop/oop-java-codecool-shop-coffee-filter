4025c947d79c93cf8ec503b8d05b1ce5290178e8
Halmai Mirella <halmaimirella@gmail.com>
2019-02-14 09:35:43 +0100

    removing implemented todos

--- src/main/java/com/codecool/shop/controller/LoginController.java
+++ src/main/java/com/codecool/shop/controller/LoginController.java
@@ -39,7 +39,6 @@ public class LoginController extends HttpServlet {
 
         UserDaoSQL uds = UserDaoSQL.getInstance();
         if (uds.checkNameAndPassword(username, password)) {
-            // todo: if items in session, add to user's cart - if came from checkout, throw back to checkout (put checkout to session?)
             HttpSession session = req.getSession();
             int userId = uds.getUserIdByUsername(username);
             session.setAttribute("username", username);
